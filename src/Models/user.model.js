6ef0de21f6c61222b7d40345a1aba659bd56b89c
Sujal Shrestha <shresthasujal309@gmail.com>
2025-06-28 22:10:12 +0545

    add async handler and bcrypt function

--- src/Models/user.model.js
+++ src/Models/user.model.js
@@ -42,4 +42,42 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this_id,
+      email: this.email,
+      userName: this.userName,
+      FullName: this.FullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this_id,
+      email: this.email,
+      userName: this.userName,
+      FullName: this.FullName,
+    },
+    process.envREFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 export const User = mongoose.model("User", userSchema);
