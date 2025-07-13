9ec592fcd91e875678347923bca4dc16e3d81180
Sujal Shrestha <shresthasujal309@gmail.com>
2025-07-13 21:28:01 +0545

    add edit and like features

--- app.js
+++ app.js
@@ -27,6 +27,30 @@ app.get("/profile", isLoggedin, async (req, res) => {
   let user = await userModel.findById(req.user.userId).populate("posts");
   res.render("profile", { user });
 });
+
+app.get("/like/:id", isLoggedin, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+
+  if (post.likes.indexOf(req.user.userId) === -1) {
+    post.likes.push(req.user.userId);
+  } else {
+    post.likes.splice(post.likes.indexOf(req.user.userId), 1);
+  }
+
+  await post.save();
+  res.redirect("/profile");
+});
+app.get("/edit/:id", isLoggedin, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+  res.render("edit", { post });
+});
+app.post("/update/:id", isLoggedin, async (req, res) => {
+  let post = await postModel.findOneAndUpdate(
+    { _id: req.params.id },
+    { content: req.body.content }
+  );
+  res.redirect("/profile");
+});
 app.post("/register", async (req, res) => {
   const { email, password, username, name, age } = req.body;
 
@@ -47,7 +71,7 @@ app.post("/register", async (req, res) => {
   const token = jwt.sign({ userId: user._id, email: user.email }, "shhhh");
   res.cookie("token", token, { httpOnly: true });
 
-  res.send("User created");
+  res.redirect("/login");
 });
 
 app.post("/login", async (req, res) => {
